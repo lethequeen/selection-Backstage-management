c71c8ce4f6f82b8e2ba98e3f59438e3a721edbab
pigyun <814728667@qq.com>
2018-02-25 15:57:18 +0800

    link

--- link.js
+++ link.js
@@ -14,28 +14,72 @@ xmlhttp1.onreadyStateChange = function () {
 xmlhttp1.send(null);
 
 //左边
-var selectcontainer = document.querySelectorAll('td');
-console.log(selectcontainer);
-selectcontainer.forEach(function (val, index) {
-  selectcontainer[index].onclick = function () {
-		var num = selectcontainer[index].querySelectorAll('ul .list');
-		for (var i=0; i < num.length; i++) {
-		  num[i].removeAttribute('class');
-		}
+var vote = document.querySelectorAll('#vote');
+console.log(vote);
+vote[0].onclick = function () {
+	var num = vote[0].querySelectorAll('ul .list');
+	for (var i=0; i < num.length; i++) {
+	  num[i].removeAttribute('class');
 	}
-})
+}
 
+var lefttd = document.getElementById('vote');
+var leftlist = document.querySelectorAll('.list');
+lefttd.onclick = function () {
+	for (var i = 0; i<leftlist.length; i++) {
+		leftlist[i].style.display = "block";
+	}
+};
 //添加投票用户
 //显示投票用户
 //删除投票用户
+var accountcontent = document.getElementById('customcontent');
+var account = document.getElementById('account');
+account.onclick = function () {
+	document.getElementById('right').style.display = "none";
+	accountcontent.style.display = "block";
+}
+var xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.open("POST", "admin/addcommon", true);
+
 //添加候选人
 //删除候选人
+var candidatecontent = document.getElementById('candidatecontent');
+var candidate = document.getElementById('candidate');
+candidate.onclick = function () {
+	candidatecontent.style.display = "block";
+}
+var xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.open("POST", "admin/addcommon", true);
+
 //设置投票开始和结束状态
 //获取投票开始和结束状态
+//某一位候选人的投票日志
+var votetimect = document.getElementById('votetimect');
+var votetime = document.getElementById('votetime');
+votetime.onclick = function () {
+	votetimect.style.display = "block";
+}
+var xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.open("POST", "admin/addcommon", true);
+
 //设置可投的最多票数 post
 //获取可投的最多票数 get
-//某一位候选人的投票日志
+var accountcontent = document.getElementById('customcontent');
+var account = document.getElementById('account');
+account.onclick = function () {
+	accountcontent.style.display = "block";
+}
+var xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.open("POST", "admin/addcommon", true);
+
 //投票公告编辑
-//
+var voteannouncect = document.getElementById('voteannouncect');
+var voteannounce = document.getElementById('voteannounce');
+voteannounce.onclick = function () {
+	accountcontent.style.display = "block";
+}
+var xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.open("POST", "admin/addcommon", true);
 
 
